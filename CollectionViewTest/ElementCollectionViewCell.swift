e40b6b7e6c6d8d5d08f46ceb0f4a5086cd30ac96
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-06-13 01:51:51 -0400

    Add UICollectionViewCell subclass.

---
+++ CollectionViewTest/ElementCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ElementCollectionViewCell.swift
+//  CollectionViewTest
+//
+//  Created by Rollin Francois on 6/13/17.
+//  Copyright © 2017 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ElementCollectionViewCell: UICollectionViewCell {
+    <#code#>
+}
+
+
+
+
